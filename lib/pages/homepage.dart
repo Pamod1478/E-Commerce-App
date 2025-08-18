30297c9e2ce34e6eaf4546a431ba98ea818f01c6
Pamod1478 <pamodsachintha2003@gmail.com>
2025-08-18 12:58:15 +0530

    addd text label and add icons

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -12,10 +12,47 @@ class HomePage extends StatelessWidget {
           preferredSize: Size.fromHeight(80),
           child: HomeAppBar(),
         ),
-        body: ListView(
-          children: [
-            // Add your page content here
-          ],
+        body: Container(
+          height: 500,
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color: Color(0xFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 5),
+                      height: 50,
+                      width: 300,
+                      color: Colors.white,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search here...",
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.camera_alt),
+                  ],
+                ),
+              ),
+              // Add more widgets here if needed
+            ],
+          ),
         ),
       ),
     );
